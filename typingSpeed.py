fa3fce8c810b256b7029998e14a779fd0c582835
AlexHanchak <ohanchak@gmail.com>
2020-10-03 12:05:01 +0300

    base

---
+++ typingSpeed.py
@@ -0,0 +1,14 @@
+#!/bin/python3
+
+import pygame
+from pygame.locals import *
+import sys
+import time
+import random
+
+
+class App:
+    def __init__(self):
+        self.width = 800
+        self.height = 500
+
